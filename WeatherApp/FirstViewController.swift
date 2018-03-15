f3b1027a31284ade1e01f64f5f7cdb8c15de6407
Moa Lenngren <moa.mail@hotmail.com>
2018-03-15 15:00:33 +0100

    Initial Commit

---
+++ WeatherApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WeatherApp
+//
+//  Created by ITHS on 2018-03-15.
+//  Copyright © 2018 MoaLenngren. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ WeatherApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  WeatherApp
+//
+//  Created by ITHS on 2018-03-15.
+//  Copyright © 2018 MoaLenngren. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
